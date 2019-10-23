c415d85cc37f9020afd99b1d3a01ed594684c6ce
brunopapait <brunohpapait@gmail.com>
2019-10-23 00:08:58 -0300

    ok

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { SafeAreaView, AsyncStorage, StyleSheet, Image } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png'
 
 export default function List() {
+    const [techs, setTechs] = useState ([]);
+
+    useEffect (() => {
+        AsyncStorage.getItem ('techs').then (storagedTechs => {
+            const techsArray = storagedTechs.split(',').map (tech => tech.trim ());
+
+            setTechs(techsArray);
+        })
+    }, [])
+
     return (
-        <View style = { styles.container }>
-            <Text>Voce esta aqui no LIST</Text>
-        </View>
+        <SafeAreaView style={ styles.container }>
+            <Image style={ styles.logo } source ={ logo } />
+
+            <SpotList/>
+        </SafeAreaView>
     );
+    
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    },
+
+    logo : {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf : "center",
+        marginTop: 30
     }
-});
+})
